4c76016d0eba7af7ead1c6ff54c0d89890d80bc1
Erik Grahn <erikgrahn13@gmail.com>
2023-12-20 20:01:52 +0100

    update setup script

--- setup-profile.ps1
+++ setup-profile.ps1
@@ -1,25 +1,24 @@
-$profileContent = @"
 # Path to the cached profile script
-\$cachedProfilePath = Join-Path (Split-Path \$PROFILE) "cachedProfile.ps1"
+$cachedProfilePath = Join-Path (Split-Path $PROFILE) "cachedProfile.ps1"
 
 # URL of the remote profile.ps1 on GitHub
-\$remoteProfileUrl = "https://raw.githubusercontent.com/erikgrahn13/pwsh-environment/main/profile.ps1"
+$remoteProfileUrl = "https://raw.githubusercontent.com/erikgrahn13/pwsh-environment/main/profile.ps1"
 
 # Function to download the latest profile script
 function Update-CachedProfile {
-    Invoke-WebRequest -Uri \$remoteProfileUrl -OutFile \$cachedProfilePath
+    Invoke-WebRequest -Uri $remoteProfileUrl -OutFile $cachedProfilePath
     Write-Host "Updated cached profile from GitHub."
 }
 
 # Check if the cached profile exists
-if (Test-Path \$cachedProfilePath) {
+if (Test-Path $cachedProfilePath) {
     # Compare the last modified time of the cached file with the current time
-    \$cachedProfileLastWriteTime = (Get-Item \$cachedProfilePath).LastWriteTime
-    \$currentTime = Get-Date
-    \$timeDifference = \$currentTime - \$cachedProfileLastWriteTime
+    $cachedProfileLastWriteTime = (Get-Item $cachedProfilePath).LastWriteTime
+    $currentTime = Get-Date
+    $timeDifference = $currentTime - $cachedProfileLastWriteTime
 
     # Update the cached profile if it's older than a day (or your preferred interval)
-    if (\$timeDifference.TotalDays -gt 1) {
+    if ($timeDifference.TotalDays -gt 1) {
         Update-CachedProfile
     }
 } else {
@@ -28,7 +27,4 @@ if (Test-Path \$cachedProfilePath) {
 }
 
 # Execute the cached profile script
-. \$cachedProfilePath
-"@
-
-Set-Content -Path $PROFILE -Value $profileContent -Force
+. $cachedProfilePath
