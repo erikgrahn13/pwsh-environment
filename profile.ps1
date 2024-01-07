b7e7012ff19519e616f74277b19be6077606a9a4
singularity <erikgrahn13@gmail.com>
2024-01-07 18:04:35 +0100

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,8 +1,7 @@
 # oh my posh needs to be installed on your system
-if ((Get-Command oh-my-posh -ErrorAction SilentlyContinue)) {
-    oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/erikgrahn13/pwsh-environment/main/mytheme.omp.json' | Invoke-Expression
-} else {
-    Write-Host "Oh My Posh is not installed."
+if (-not (Get-Command starship -ErrorAction SilentlyContinue)) {
+    Write-Host "Starship is not installed."
+    exit
 }
 
 # Check if Terminal-Icons module is installed
@@ -13,16 +12,18 @@ if (-not (Get-Module -ListAvailable -Name Terminal-Icons)) {
 Import-Module Terminal-Icons
 
 # Check if posh-git is installed
-# if (-not (Get-Module -ListAvailable -Name posh-git)) {
-#     # Install posh-git
-#     Write-Host "Installing posh-git..."
-#     Install-Module posh-git -Scope CurrentUser -Force
-#     Write-Host "posh-git installed."
-# }
+if (-not (Get-Module -ListAvailable -Name posh-git)) {
+    # Install posh-git
+    Write-Host "Installing posh-git..."
+    Install-Module posh-git -Scope CurrentUser -Force
+    Write-Host "posh-git installed."
+}
 
-# Import-Module posh-git
+Import-Module posh-git
 
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineOption -PredictionSource History
 # Set-PSReadLineOption -PredictionViewStyle ListView
+Invoke-Expression (&starship init powershell)
+
