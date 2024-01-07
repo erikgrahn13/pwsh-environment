8a903e24ff074d81a9d33629b4e67daae30ecd4c
singularity <erikgrahn13@gmail.com>
2024-01-07 18:30:49 +0100

    Update setup-profile.ps1

--- setup-profile.ps1
+++ setup-profile.ps1
@@ -31,16 +31,16 @@ Write-Host "PowerShell profile has been set up."
 
 # copy the starship config
 # Define the URL of the raw file from the GitHub repository
-$fileUrl = "https://raw.githubusercontent.com/erikgrahn13/shell-environment/main/starship.toml"
+# $fileUrl = "https://raw.githubusercontent.com/erikgrahn13/shell-environment/main/starship.toml"
 
-# Define the destination directory and file name within the user's home directory
-$destDir = Join-Path $HOME ".config"  # e.g., $HOME\MyFiles
-$destFile = Join-Path $destDir "starship.toml"  # e.g., $HOME\MyFiles\filename
+# # Define the destination directory and file name within the user's home directory
+# $destDir = Join-Path $HOME ".config"  # e.g., $HOME\MyFiles
+# $destFile = Join-Path $destDir "starship.toml"  # e.g., $HOME\MyFiles\filename
 
-# Check if the directory exists
-if (-not (Test-Path $destDir)) {
-    # The directory doesn't exist, so create it
-    New-Item -ItemType Directory -Path $destDir
-}
+# # Check if the directory exists
+# if (-not (Test-Path $destDir)) {
+#     # The directory doesn't exist, so create it
+#     New-Item -ItemType Directory -Path $destDir
+# }
 
-Invoke-WebRequest -Uri $fileUrl -OutFile $destFile
+# Invoke-WebRequest -Uri $fileUrl -OutFile $destFile
